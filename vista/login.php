0e014fbd4baf76821ee6a6a47ed1ef5f17e0d9f6
Santi <meimeisps@gmail.com>
2023-01-24 13:15:50 +0100

    cambios en registro

--- vista/login.php
+++ vista/login.php
@@ -1,24 +1,164 @@
-<form action="./controlador/CtrlLogin.php" method="POST">
-    <label for="usu">Usuario:</label>
-    <input type="text" name="NombreUsuario" id="NombreUsuario" autofocus>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro</title>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> -->
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="web/styles/login.css">
+    
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="web/styles/style.css">
 
-    <label for="pass">Contraseña:</label>
-    <input type="password" name="Contraseña" id="Contraseña">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-    <input type="submit" value="Iniciar sesión">
-    <!-- <p>¿No tienes una cuenta? <a href="./vista/register.php">Regístrate ahora</a>.</p>   -->
-    <!-- asi permitirá acceder a register desde la ventana principal de localhost/prueba -->
+</head>
 
 
-    <p>¿No tienes una cuenta? <a href="./vista/register.php">Regístrate ahora</a>.</p>
+<body id="page-top">
 
-    <!-- así permitirá acceder a register desde la ventana prueba/vista -->
+<!-- navigator -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
 
-    <!-- no es lo mismo la dirección inicial que carga el ctrlMain del login.php que cuando la direccionamos desde el link de registro. -->
-</form>
+        <div class="container">
+            <a class="navbar-brand" href="#page-top"><img src="web/img/logo2.png" alt="logo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="menu.html">Our Menu</a></li> <!--FALTA PONER HREF CON RESTO DE PÁGINAS, NO #x-->
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Order Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">Book Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./vista/register.php">Register</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <br><br> <br><br>     <br><br>
+<!-- navigator -->
+
+
+<div class="container">
+<br>  
+<!-- <p class="text-center" style="color:white;">Logo</p> -->
+<hr>
 
 
 
 
 
+<div class="card bg-dar" >
+
+<article class="card-body mx-auto articulo" id="bg-article">
+	<h4 class="card-title mt-3 text-center text-uppercase" style="color:white; max-width: 400px; font-family: Montserrat;" >Crear cuenta</h4>
+	<!-- <p class="text-center text-uppercase " style="color:white;">Disfruta de tu pedido en casa.</p> -->
+
+
+    <form action="./controlador/CtrlLogin.php" method="POST" class="needs-validation" novalidate>
+
+        
+
+    <!-- form group -->
+ 
+            
       
+            <div class="form-group input-group ">
+
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+
+            </div>
+            <input type="text"  class="form-control" name="NombreUsuario" id="NombreUsuario" required placeholder="Nombre de Usuario"autofocus >
+            <div class="invalid-feedback">
+            Please choose a username.
+            </div>
+            </div>
+
+
+
+            	<!-- form-group// -->
+
+                <div class="form-group input-group">
+
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+
+            </div>
+
+
+            <input type="password" class="form-control" name="Contraseña" id="Contraseña"placeholder="Contraseña">
+</div>
+
+
+            <!-- form-group// -->
+
+
+
+  
+
+
+
+        
+
+
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary btn-block" value="Ingresar">
+                <input type="reset" class="btn btn-warning btn-block" value="Borrar">
+            </div>
+            <p class="text-center" style="color:white;">Aún no tienes una cuenta? <a href="./vista/register.php" class="btn btn-danger" >REGISTRATE!</a> </p>   
+        </form>
+        </article>
+        </div>    
+        </div>
+
+            <br><br>     <br><br>     <br><br>     <br><br>     <br><br>
+
+    <footer class="bg-dark text-center text-white">
+        <div class="container p-4">
+            <section class="mb-4">
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i
+                        class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i
+                        class="fab fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i class="fab fa-linkedin"></i></a>
+            </section>
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                Contact us at: <a class="text-light" href="mailto:nigirijapan@gmail.com">nigirijapan@gmail.com</a> | <a
+                    class="text-light" href="tel:+34963123456">963 12 34 56</a> | <a class="text-light"
+                    href="https://goo.gl/maps/x7VUrEzaKU3dV3cV8">C/D'Alberic 18, 46008 València</a>
+            </div>
+    </footer>
+    <script>
+        window.onscroll = function () { scrollFunction() };
+        function scrollFunction() {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                document.querySelector("nav").classList.add("navbar-shrink");
+            } else {
+                document.querySelector("nav").classList.remove("navbar-shrink");
+            }
+        }
+    </script>
+    <script src="validate-forms.js"></script>
+    </body>
+
+    
+
+
+
+</html>
--- vista/register.php
+++ vista/register.php
@@ -98,12 +98,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $direccion = trim($_POST["direccion"]);
     }
 
-    // if(empty(trim($_POST["localidad"]))){
-    //     $localidad_err="porfavor , introduzca su localidad.";
-    // }else{
-    //     $localidad = trim($_POST["localidad"]);
-    // }
-    
+ 
 
 $listaProvincias = trim($_POST["provincia"]);
 
@@ -152,68 +147,204 @@ $listaProvincias = trim($_POST["provincia"]);
 <head>
     <meta charset="UTF-8">
     <title>Registro</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../web/styles/login.css">
+    
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../web/styles/style.css">
+
+
 </head>
-<body>
-    <div class="wrapper">
-        <h2>Nuevo usuario.</h2>
-        <p>crea una cuenta y disfruta ahora.</p>
+
+
+<body id="page-top">
+
+<!-- navigator -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+
+        <div class="container">
+            <a class="navbar-brand" href="#page-top"><img src="../web/img/logo2.png" alt="logo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="menu.html">Our Menu</a></li> <!--FALTA PONER HREF CON RESTO DE PÁGINAS, NO #x-->
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Order Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">Book Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../hugo.php">Log in</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <br><br> <br><br>
+<!-- navigator -->
+
+<div class="container">
+<br>  
+<!-- <p class="text-center" style="color:white;">Logo</p> -->
+<hr>
+
+
+
+
+
+<div class="card bg-dar bg-dark" >
+
+<article class="card-body mx-auto articulo" id="bg-article">
+	<h4 class="card-title mt-3 text-center text-uppercase" style="color:white; max-width: 400px; font-family: Montserrat;" >Crear cuenta</h4>
+	<!-- <p class="text-center text-uppercase " style="color:white;">Disfruta de tu pedido en casa.</p> -->
+
+
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Usuario</label>
-                <input type="text" name="NombreUsuario" class="form-control" value="<?php echo $NombreUsuario; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($mail_err)) ? 'has-error' : ''; ?>">
-                <label>E-mail</label>
-                <input type="email" name="email" class="form-control" value="<?php echo $mail; ?>">
-                <span class="help-block"><?php echo $mail_err; ?></span>
-            </div>   
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Contraseña</label>
-                <input type="password" name="Contraseña" class="form-control" value="<?php echo $Contraseña; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirmar Contraseña</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+
+        <h4 class="card-title mt-3 text-center text-uppercase " style="color:white; max-width: 400px; font-family: Montserrat;" >Datos de usuario</h4>
+
+    <!-- form group -->
+            <div class="form-group input-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+			
             </div>
 
+                <input type="text" name="NombreUsuario" class="form-control is-invalid" placeholder="Nombre usuario" value="<?php echo $NombreUsuario; ?>">
+               
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
 
-            <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-                <label>Nombre</label>
-                <input type="text" name="Nombre" class="form-control" value="<?php echo $nombre; ?>">
-                <span class="help-block"><?php echo $name_err; ?></span>
             </div>    
-            <div class="form-group <?php echo (!empty($apellidos_err)) ? 'has-error' : ''; ?>">
-                <label>Apellidos</label>
-                <input type="text" name="apellidos" class="form-control" value="<?php echo $apellidos; ?>">
-                <span class="help-block"><?php echo $apellidos_err; ?></span>
+
+	<!-- form-group// -->
+
+            <div class="form-group  input-group<?php echo (!empty($mail_err)) ? 'has-error' : ''; ?>">
+               
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+
+            </div>
+
+                <input type="email" name="email" class="form-control  is-invalid" placeholder="E-mail" value="<?php echo $mail; ?>">
+             
+                <span class="invalid-feedback"><?php echo $mail_err; ?></span>
+
             </div>   
-            <div class="form-group <?php echo (!empty($direccion_err)) ? 'has-error' : ''; ?>">
-                <label>Dirección</label>
-                <input type="text" name="direccion" class="form-control" value="<?php echo $direccion; ?>">
-                <span class="help-block"><?php echo $direccion_err; ?></span>
+
+            	<!-- form-group// -->
+
+
+            
+            	
+
+
+                <div class="form-group input-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+
+                <div class="input-group-prepend">
+
+                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+
+                </div>
+
+
+             <input type="password" name="Contraseña" class="form-control  is-invalid" placeholder="contraseña" value="<?php echo $Contraseña; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+
+            <!-- form-group// -->
+
+
+
+
+
+            <div class="form-group input-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+
+            </div>
+
+                <input type="password" name="confirm_password" class="form-control  is-invalid" placeholder="Repetir contraseña" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+
+            
+
+            	<!-- form-group// -->
+
+                <h4 class="card-title mt-3 text-center text-uppercase titulo" style="color:white; max-width: 400px; font-family: Montserrat;" >Datos personales</h4>
+
+            <div class="form-group input-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+
+            </div>
+
+                <input type="text" name="Nombre" class="form-control  is-invalid"  placeholder="Nombre" value="<?php echo $nombre; ?>">
+                <span class="invalid-feedback"><?php echo $name_err; ?></span>
             </div>    
-            <!-- <div class="form-group <?php echo (!empty($localidad_err)) ? 'has-error' : ''; ?>">
-                <label>Localidad</label>
-                <input type="text" name="localidad" class="form-control" value="<?php echo $localidad; ?>">
-                <span class="help-block"><?php echo $localidad_err; ?></span>
-            </div>    -->
 
-     
+            	<!-- form-group// -->
 
-            <div class="form-group">
-                <label>Provincia</label>
-                <select name="provincia">
+            <div class="form-group input-group <?php echo (!empty($apellidos_err)) ? 'has-error' : ''; ?>">
+                
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+
+            </div>
+
+                <input type="text" name="apellidos" class="form-control  is-invalid" placeholder="Apellidos" value="<?php echo $apellidos; ?>">
+                <span class="invalid-feedback"><?php echo $apellidos_err; ?></span>
+            </div>   
+
+            	<!-- form-group// -->
+
+            <div class="form-group input-group <?php echo (!empty($direccion_err)) ? 'has-error' : ''; ?>">
+                
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-address-card "></i> </span>
+
+            </div>
+
+                <input type="text" name="direccion" class="form-control  is-invalid"  placeholder="Dirección" value="<?php echo $direccion; ?>">
+                <span class="invalid-feedback"><?php echo $direccion_err; ?></span>
+            </div>    
+
+
+
+        	<!-- form-group// -->
+
+            <div class="form-group input-group">
+                
+            <div class="input-group-prepend">
+
+            <span class="input-group-text"> <i class="fa fa-address-card"></i> </span>
+
+            </div>
+
+                <select  class="form-control" name="provincia">
+                <option selected="">Provincia</option>
 
                 <?php 
                 $listaProvincias = ['Alava', 'Albacete', 'Alicante', 'Almería', 'Asturias' ,'Avila', 'Badajoz', 'Barcelona', 'Burgos', 'Cáceres', 'Guipúzcoa', 'Huelva', 'Huesca', 'Islas Baleares', 'Jaén', 'León', 'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Murcia', 'Navarra', 'Orense', 'Palencia', 'Las Palmas', 'Pontevedra', 'La Rioja', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora', 'Zaragoza'];
+
                 echo count($listaProvincias);
                 for ($i=0; $i < count($listaProvincias); $i++) { 
                     echo "<option value=\"".$listaProvincias[$i]."\"";
@@ -221,17 +352,55 @@ $listaProvincias = trim($_POST["provincia"]);
                     echo ">".$listaProvincias[$i];
                 }
                 ?>
+
                 </select>
 
             </div>
 
 
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Ingresar">
-                <input type="reset" class="btn btn-default" value="Borrar">
+                <input type="submit" class="btn btn-primary btn-block" value="Ingresar">
+                <input type="reset" class="btn btn-warning btn-block" value="Borrar">
             </div>
-            <p>¿Ya tienes una cuenta? <a href="../hugo.php">Ingresa aquí</a>.</p>
+            <p class="text-center" style="color:white;">Ya tienes una cuenta? <a href="../hugo.php" class="btn btn-danger" >Iniciar sesión</a> </p>   
         </form>
-    </div>    
-</body>
+            </article>
+        </div>    
+            </div>
+
+            <br><br>
+
+    <footer class="bg-dark text-center text-white">
+        <div class="container p-4">
+            <section class="mb-4">
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i
+                        class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i
+                        class="fab fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"><i class="fab fa-linkedin"></i></a>
+            </section>
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                Contact us at: <a class="text-light" href="mailto:nigirijapan@gmail.com">nigirijapan@gmail.com</a> | <a
+                    class="text-light" href="tel:+34963123456">963 12 34 56</a> | <a class="text-light"
+                    href="https://goo.gl/maps/x7VUrEzaKU3dV3cV8">C/D'Alberic 18, 46008 València</a>
+            </div>
+    </footer>
+    <script>
+        window.onscroll = function () { scrollFunction() };
+        function scrollFunction() {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                document.querySelector("nav").classList.add("navbar-shrink");
+            } else {
+                document.querySelector("nav").classList.remove("navbar-shrink");
+            }
+        }
+    </script>
+
+    </body>
+
+    
+
+
+
 </html>
