879106678ee66b1acd549b4ec94edae5503b81d6
RaulGit123 <r.rodriguezmin@gmail.com>
2023-01-27 08:18:28 +0100

    Arreglado footer reservas

--- web/reservas.php
+++ web/reservas.php
@@ -1,35 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-    <title>Registro</title>
-    <!--     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> -->
-     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" /> -->
-	<link rel="stylesheet" href="../web/styles/login.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"> -->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"> -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
-    <link rel="stylesheet" href="../web/styles/book.css">
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="styles/book.css">
 </head>
 <body id="page-top">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand" href="index.php"><img src="../web/img/logo2.png" alt="logo" /></a>
+            <a class="navbar-brand" href="index.php"><img src="img/logo2.png" alt="logo" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                
-                <i class="fas fa-bars ms-1"></i>
+                <i class="fas fa-bars my-1"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav text-uppercase ml-auto py-4 py-lg-0">
-                   
-                <?php
+                    <?php
                         
                         if (session_status()===PHP_SESSION_NONE){
                             session_start();
@@ -40,43 +33,47 @@
                             <li class="nav-item"><a class="nav-link" href="pedidos.php">Order Now</a></li>
                             <li class="nav-item"><a class="nav-link" href="#page-top">Book Now</a></li>
                             <li class="nav-item"><a class="nav-link" href="../controlador/CtrlSalir.php"><?php echo"Bienvenido ".$_SESSION["NombreUsuario"];?></a></li><?php
+                        }else {
+                            ?>
+                            <li class="nav-item"><a class="nav-link" href="menu.php">Our Menu</a></li> <!--FALTA PONER HREF CON RESTO DE PÁGINAS, NO #x-->
+                            <li class="nav-item"><a class="nav-link" href="../vista/principal.php">Order Now</a></li>
+                            <li class="nav-item"><a class="nav-link" href="../vista/principal.php">Book Now</a></li>
+                            <li class="nav-item"><a class="nav-link" href="../hugo.php">Log in</a></li><?php
                         }
                         ?>
-                            
-                            </ul>
+                        <!-- href="../controlador/CtrlSalir.php"> referenciará a finalizar la sesión -->
+                </ul>
             </div>
         </div>
-    </nav> 
-   
+    </nav>
     <header class="masthead">
         <div class="px-0 container">
             <div class="masthead-subheading font-italic">Book a table and enjoy your favorite Japanese food </div>
             <br>
             <br>
-            <!-- <div class="mt-3 masthead-heading text-uppercase">Booking</div> -->
         </div>
     </header>
-    <div class="card bg-dar" >
-<!-- Cuanta gente va a acudir  -->
-<article class="card-body mx-auto articulo" id="bg-article">
-	<h4 class="card-title mt-3 text-center text-uppercase">Booking</h4>
-	 <p class="text-center text-uppercase people">People</p>
+    <section class="card bg-dar">
+        <!-- Cuanta gente va a acudir  -->
+        <div class="card-body mx-auto articulo" id="bg-article"> 
+        <h4 class="card-title mt-3 text-center text-uppercase">Booking</h4>
+        <p class="text-center text-uppercase people">People</p>
      <div class="peopleselected ">
      <button class="btoMa" name="button">+</button>
      <div class="cantidad text-center">0</div>
      <button class="btoMe" name="button">-</button>
     </div>
-        </article>
-<!-- La fecha en la cual van a acudir -->
-        <article class="card-body mx-auto articulo" id="bg-article">
-	 <p class="text-center text-uppercase people">Date</p>
+                </div>
+            <!-- La fecha en la cual van a acudir -->
+            <div class="card-body mx-auto articulo" id="bg-article">
+            <p class="text-center text-uppercase people">Date</p>
      <div class="dateselected ">
      <input type="date"class="text-center fecha" size="20"/>
     </div>
-        </article>
-<!-- La hora a la que quieren acudir(y puedan) -->
-        <article class="card-body mx-auto articulo" id="bg-article">
-	 <p class="text-center text-uppercase people">Hours available</p>
+            </div>
+            <!-- La hora a la que quieren acudir(y puedan) -->
+            <div class="card-body mx-auto articulo" id="bg-article">
+            <p class="text-center text-uppercase people">Hours available</p>
      <div class="hourselected grid">
      <button class="item text-center">
              <div class="horas">13:30</div>
@@ -103,9 +100,8 @@
              <div class="horas">22:30</div>           
      </button>
     </div>
-        </article>
-
-        </div> 
+            </div>
+    </section>
     <footer class="bg-dark text-center text-white">
         <div class="container p-4">
             <section class="mb-4">
@@ -136,9 +132,3 @@
     </script>
 </body>
 </html>
-                            
-                            
-                        
-                       
-                        <!-- href="../controlador/CtrlSalir.php"> referenciará a finalizar la sesión -->
-     
