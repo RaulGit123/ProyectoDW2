70f540677bf6d9e544b0ec27c099b2709710dd33
Diego <diegodiego@gmail.com>
2023-01-31 14:41:18 +0100

    cambios

--- web/GuardaReserva.php
+++ web/GuardaReserva.php
@@ -20,23 +20,18 @@ $sql = "SELECT IdUsuarios FROM usuarios WHERE NombreUsuario = '$nombre'";
 $query = $con -> prepare($sql); 
 $query -> execute(); 
 $results = $query -> fetchAll(PDO::FETCH_OBJ); 
-echo '<table border="0" cellspacing="2" cellpadding="2"> 
-     <tr> 
-          <td> <font face="Arial">IdUsuario</font> </td> 
-  
-  </tr>';
+
 
 if($query -> rowCount() > 0)   { 
 foreach($results as $result) { 
-echo " <tr> 
-<td>".$result -> IdUsuarios."</td>
 
+$id= $result -> IdUsuarios;
 
-</tr>";
 
 
    }
  }
+ 
 
 function AñadirRegistro($id,$output,$output2,$output3)
 {
@@ -46,5 +41,5 @@ function AñadirRegistro($id,$output,$output2,$output3)
     ]);
 }
 
-//AñadirRegistro($output,$output2,$output3);
+AñadirRegistro($id,$output,$output2,$output3);
 ?>  
