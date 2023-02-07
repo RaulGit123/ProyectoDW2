c6a5fa48e24f594617bb2db7da4aa449ce78eb1a
Santi <meimeisps@gmail.com>
2023-02-07 12:16:32 +0100

    retoques de formularios

--- vista/verificar.php
+++ vista/verificar.php
@@ -28,6 +28,25 @@
         <body id="page-top">
 
 
+    <nav class="stroke navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+
+        <div class="container">
+            <a class="navbar-brand" href="../index.php"><!-- href . lleva a web/index.php --><img src="../web/img/logo2.png" alt="logo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="../web/menu.php">Our Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./principal.php">Order Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./principal.php">Book Now</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./register.php">Register</a></li>
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
     
 
 
