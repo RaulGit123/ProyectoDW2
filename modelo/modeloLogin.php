9b78d1aa67f808e74a845f004366dd12b4902b83
RaulGit123 <r.rodriguezmin@gmail.com>
2023-02-09 09:49:35 +0100

    d

--- modelo/modeloLogin.php
+++ modelo/modeloLogin.php
@@ -41,10 +41,11 @@ include ("ClaseNigiri.php");
 
                 if ($cantidad_resultado == 1) {
                     $_SESSION["NombreUsuario"] = $usu;
-                    $_SESSION["Contraseña"] = $pass;  
-                 
-
-
+                    $_SESSION["Contraseña"] = $pass;
+                echo $_SESSION["Contraseña"];
+                echo $_SESSION["NombreUsuario"];
+                echo $usu;
+                echo $pass;
                 } else {
                     $_SESSION["error"] = "ERROR";
 
@@ -60,9 +61,9 @@ include ("ClaseNigiri.php");
                 $sql = null;
                 $resultado = null;
                 $cantidad_resultado = null;
-                echo $_POST["NombreUsuario"];
-                echo $_POST["Contraseña"];
-            echo "holita";
+            //     echo $_POST["NombreUsuario"];
+            //     echo $_POST["Contraseña"];
+            // echo "holita";
                 // header("location:../modelo/sesion.php");
 
             }
