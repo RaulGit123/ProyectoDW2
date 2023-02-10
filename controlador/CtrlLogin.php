ae76e8a3fdc4ff19b02954e06d6ef1972ffba12d
Santi <meimeisps@gmail.com>
2023-02-10 13:43:30 +0100

    comentarios

--- controlador/CtrlLogin.php
+++ controlador/CtrlLogin.php
@@ -1,11 +1,13 @@
 <?php
-echo $_POST["NombreUsuario"];
-    if (isset($_POST["NombreUsuario"]) && isset($_POST["Contraseña"])) {
-         require_once("../modelo/modeloLogin.php");
-        $validar = new Login();
-        $validar->validarDatos($_POST["NombreUsuario"], $_POST["Contraseña"]);
-        
-    } else {
-        header("location:../web/index.php");
-    }
-?>
+
+//CtrlLogin servirá de controlador para el login.  Este comparará si está seteado de post el usuario y contraseña
+
+
+if (isset($_POST["NombreUsuario"]) && isset($_POST["Contraseña"])) {
+    require_once("../modelo/modeloLogin.php");
+    $validar = new Login();                                                     //hacemos un nuevo login de la clase de modeloLogin
+    $validar->validarDatos($_POST["NombreUsuario"], $_POST["Contraseña"]);      //llamamos a la funcion validarDatos pasandole las variables de post.
+
+} else {                                                                        //si no se cumple la condición devolverá al index.
+    header("location:../web/index.php");
+}
--- controlador/CtrlMain.php
+++ controlador/CtrlMain.php
@@ -1,26 +1,26 @@
 <?php
-    session_start();
-    if (isset($_SESSION["NombreUsuario"]) && isset($_SESSION["Contraseña"]) ) {
-        require_once("Validar.php");
-        $validar = new Validar();
+    session_start();              //invocamos session_start para iniciar la sesion
+    if (isset($_SESSION["NombreUsuario"]) && isset($_SESSION["Contraseña"]) ) {  //comprobamos si está sesion setteado
+        require_once("Validar.php");                                                                
+        $validar = new Validar();        //creamos un nuevo objeto validar donde llamaremos a la función para validar los campos
         $validar->validarDatos();
       
-        $nombre = $_SESSION["NombreUsuario"];
-        $con = Conexion::getConection();
-
-        $sql = "SELECT Activado FROM Usuarios WHERE NombreUsuario = '$nombre'";
+        $nombre = $_SESSION["NombreUsuario"];   //capturamos la variable nombreUsuario del usuario de la sesión para comparar luego.
+        $con = Conexion::getConection();  //creamos una nueva conexion con la funcion creada en la clase conexion que hereda de la hoja conexion-php que es requerida en validar.php
+        /* Hacemos una consulta para ver si el usuario que ha iniciado sesión está verificado.*/
+        $sql = "SELECT Activado FROM Usuarios WHERE NombreUsuario = '$nombre'"; 
         $query = $con -> prepare($sql); 
         $query -> execute(); 
-        $results = $query -> fetchAll(PDO::FETCH_OBJ);
+        $results = $query -> fetchAll(PDO::FETCH_OBJ);    //se guardan los resultados en un array
         if($query -> rowCount() > 0)   { 
         foreach($results as $result) { 
-            echo '<div id="idUsu" style="display: none;">'.$result -> Activado.'</div>';
+            echo '<div id="idUsu" style="display: none;">'.$result -> Activado.'</div>';  // se recorre en un foreach para comparar todos los códigos.
         }
         }
-        $activado = $result->Activado;
-
+        $activado = $result->Activado;  
+// este condicional enviará a páginas privadas al usuario si es admin,usuario validado o no validado.
         if($_SESSION["NombreUsuario"]=="admin" ){
-                header('Location: ../web/admin.php');}
+                header('Location: ../web/admin.php');}   
                 
         if($_SESSION["NombreUsuario"]!="admin" && $activado=='si'){
                 include_once("../web/principal.php");}   
@@ -29,7 +29,7 @@
             echo '<script language="javascript">alert("Error de verificación");</script>'; }
             
             
-     
+     //si no está seteada la sesión devolverá un error
     } else {
         if (isset($_SESSION["error"])) {
             echo '<script language="javascript">alert("Error log in");</script>';
@@ -38,6 +38,5 @@
     
         include_once("../web/login.php");
 
-        //aqui luego desviariamos a vista registro de usuario.
+ 
     }
-?>
