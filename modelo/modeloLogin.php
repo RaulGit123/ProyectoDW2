c5fb3dbd35a351570706ee33b115b2d5ccafa21a
[Santi] <[meimeisps@gmail.com]>
2023-02-10 16:15:26 +0100

    comentarios y formato

--- modelo/modeloLogin.php
+++ modelo/modeloLogin.php
@@ -1,66 +1,55 @@
-<?php 
-include ("ClaseModelo.php");
-include ("ClaseNigiri.php");
-    require_once("Conexion.php");
+<?php
+include("ClaseModelo.php");
+include("ClaseNigiri.php");
+require_once("Conexion.php");
+//Clase login con funcion que validará un nombre y usuario para entrar en la sesión
+//Clase requerida por CtrlLogin para hacer las comprobaciones
+class Login
+{
 
-    class Login {
+    public function validarDatos($usu, $pass)
+    {
 
-        public function validarDatos($usu, $pass) {
-       
-           $pass=md5($pass);
-           
-            try {
+        $pass = md5($pass);
 
-                $con = null;
-                $sql = null;
-                $resultado = null;
-                $cantidad_resultado = null;
-                $activado = '';
+        try {
 
-                // Recuperamos la conexión
-                $con = Conexion::getConection();
+            $con = null;
+            $sql = null;
+            $resultado = null;
+            $cantidad_resultado = null;
+            $activado = '';
 
-                // Validación de error
-                if ($con == "ERROR") {
-                    header("location:CtrlSalir.php");
-                }
+            // Recuperamos la conexión
+            $con = Conexion::getConection();
 
-                // Consulta
-                // $sql = "SELECT * FROM usuarios WHERE NombreUsuario = :USU AND Contraseña = :PASS";
-
-                // $resultado = $con->prepare($sql);
-                // $resultado->execute(array(":USU"=>$usu, ":PASS"=>$pass));
-
-                $f1 = new Nigiri();
-            $resultado = $f1->Login($usu,$pass);
-
-
-                $cantidad_resultado = $resultado->rowCount();
-                 session_start();
-            // $cantidad_resultado = 1;
-                if ($cantidad_resultado == 1) {
-                    $_SESSION["NombreUsuario"] = $usu;
-                    $_SESSION["Contraseña"] = $pass;
-                } else {
-                    $_SESSION["error"] = "ERROR";
-
-                }
-
-                
-            } catch (Exception $e) {
-
-
-            } finally {
-
-                $con = null;
-                $sql = null;
-                $resultado = null;
-                $cantidad_resultado = null;
-                 header("location:../modelo/sesion.php");
-
+            // Validación de error
+            if ($con == "ERROR") {
+                header("location:CtrlSalir.php");
             }
 
-        }
 
+
+            $f1 = new Nigiri();
+            $resultado = $f1->Login($usu, $pass);
+
+
+            $cantidad_resultado = $resultado->rowCount();
+            session_start();
+            if ($cantidad_resultado == 1) {
+                $_SESSION["NombreUsuario"] = $usu;
+                $_SESSION["Contraseña"] = $pass;
+            } else {
+                $_SESSION["error"] = "ERROR";
+            }
+        } catch (Exception $e) {
+        } finally {
+
+            $con = null;
+            $sql = null;
+            $resultado = null;
+            $cantidad_resultado = null;
+            header("location:../modelo/sesion.php");
+        }
     }
-?>
+}
--- modelo/verificado.php
+++ modelo/verificado.php
@@ -4,28 +4,24 @@ require_once("Conexion.php");
 //Saber el código asignado al usuario
 $con = Conexion::getConection();
 $sql = "SELECT Codigo FROM Usuarios";
-$query = $con -> prepare($sql); 
-$query -> execute(); 
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$query = $con->prepare($sql);
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_OBJ);
 
-if($query -> rowCount() > 0)   { 
-  foreach($results as $result) { 
-    $codigo= $result -> Codigo;
-    
-    //Si coinciden códigos, se activa la cuenta
-    if(trim($_POST["codigo"])==  $codigo){
+if ($query->rowCount() > 0) {
+  foreach ($results as $result) {
+    $codigo = $result->Codigo;
+
+    //Si coinciden códigos, se activa la cuenta y se envia a sesion.php
+    if (trim($_POST["codigo"]) ==  $codigo) {
       $con = Conexion::getConection();
       $sql = "UPDATE Usuarios SET Activado = 'si' WHERE Codigo ='$codigo' ";
-      $query = $con -> prepare($sql); 
-      $query -> execute(); 
-     
-      header("location:sesion.php");
-    }else{
-      header("location:../web/verificar.php");
+      $query = $con->prepare($sql);
+      $query->execute();
 
+      header("location:sesion.php");
+    } else { // si no coinciden se devuelve a login verificar.
+      header("location:../web/verificar.php");
     }
   }
 }
-
-?>
-
